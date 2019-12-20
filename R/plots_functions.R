5960a442d41c324929448dc77b89698894f1836b
RamiKrispin <ramkrisp@umich.edu>
2019-12-20 02:50:56 -0800

    Update the margin setting

--- R/plots_functions.R
+++ R/plots_functions.R
@@ -47,7 +47,8 @@ plot_res <- function(model, na.rm = FALSE, margin = 0.04){
                    yaxis = list(title = "Count"))
 
   p_output <- plotly::subplot(plotly::subplot(p1, p2, nrows = 2, shareX = T),
-                              plotly::subplot(p3$residuals$plot, p4, nrows = 1, titleY = T, titleX = T ), nrows = 2, titleY = T, titleX = T, margin = margin,
+                              plotly::subplot(p3$residuals$plot, p4, nrows = 1, titleY = T, titleX = T, margin = margin ),
+                              nrows = 2, titleY = T, titleX = T, margin = margin,
                               heights = c(0.6, 0.4)) %>%
     plotly::hide_legend() %>%
     plotly::layout(title = "Residuals Analysis")
